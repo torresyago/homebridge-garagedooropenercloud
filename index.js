1f3536dca6c72b4dd019cd62f257e39a713f15c7
torresyago <mcubqcg@gmail.com>
2025-12-24 15:01:48 +0100

    Ajuste status universal

--- index.js
+++ index.js
@@ -27,31 +27,31 @@ function GarageDoorOpener(log, config) {
     this.closeTime = config.closeTime || 20;
     this.polling = config.polling !== false;
     this.debug = config.debug !== false;
-    
+
     this.statusCloudURL = "https://shelly-38-eu.shelly.cloud/device/status";
     this.controlCloudURL = "https://shelly-38-eu.shelly.cloud/device/relay/control";
-    
+
     this.service = new Service.GarageDoorOpener(this.name);
     this.informationService = new Service.AccessoryInformation();
-    
+
     this.informationService
         .setCharacteristic(Characteristic.Manufacturer, "Shelly Cloud")
         .setCharacteristic(Characteristic.Model, "Garage Door Opener")
         .setCharacteristic(Characteristic.SerialNumber, this.deviceId);
-    
+
     this.service
         .getCharacteristic(Characteristic.TargetDoorState)
         .on('set', this.setTargetState.bind(this));
-    
+
     this.service
         .getCharacteristic(Characteristic.CurrentDoorState)
         .on('get', this.getCurrentState.bind(this));
-    
+
     if (this.polling) {
         setInterval(this.pollStatus.bind(this), 30000);
         this.pollStatus();
     }
-    
+
     this.log("[%s] Initializing GarageDoorOpener accessory...", this.name);
 }
 
@@ -59,17 +59,17 @@ GarageDoorOpener.prototype = {
     getServices: function() {
         return [this.informationService, this.service];
     },
-    
+
     setTargetState: function(targetState, callback) {
         this.log("[%s] Target state: %s", this.name, targetState);
-        
+
         const toggleData = {
             id: this.deviceId,
             channel: this.channel,
             auth_key: this.authKey,
             turn: "toggle"
         };
-        
+
         request.post({
             url: this.controlCloudURL,
             form: toggleData
@@ -79,12 +79,12 @@ GarageDoorOpener.prototype = {
                 callback(err);
                 return;
             }
-            
+
             this.log("[%s] Toggle command sent", this.name);
             callback(null);
         });
     },
-    
+
     getCurrentState: function(callback) {
         this.getStatus((err, status) => {
             if (err) {
@@ -92,7 +92,7 @@ GarageDoorOpener.prototype = {
                 callback(err);
                 return;
             }
-            
+
             let currentState;
             if (status === undefined || status === null || status === false) {
                 currentState = Characteristic.CurrentDoorState.CLOSED;
@@ -104,18 +104,18 @@ GarageDoorOpener.prototype = {
                 currentState = Characteristic.CurrentDoorState.STOPPED;
                 this.log("[%s] Status: %s -> HomeKit: STOPPED (4)", this.name, status);
             }
-            
+
             callback(null, currentState);
         });
     },
-    
+
     getStatus: function(callback) {
         const statusData = {
             channel: this.channel,
             id: this.deviceId,
             auth_key: this.authKey
         };
-        
+
         request.post({
             url: this.statusCloudURL,
             form: statusData
@@ -124,38 +124,56 @@ GarageDoorOpener.prototype = {
                 callback(err);
                 return;
             }
-            
+
             try {
                 const json = JSON.parse(body);
                 if (this.debug) {
                     this.log("[%s] FULL JSON: %j", this.name, json);
                 }
-                
-                const rawStatus = json.data.device_status.relays[0]?.ison ?? false;
-                const status = rawStatus;
-                this.log("[%s] RAW RELAY: %s", this.name, status);
-                
+
+                // *** FIX UNIVERSAL - Soporta TODOS los Shelly ***
+                const ds = json ? json.data ? json.data.device_status : null : null;
+                let status = false;
+
+                if (ds && ds['input:0'] && ds['input:0'].state !== undefined) {
+                    status = ds['input:0'].state;
+                    this.log("[%s] Using input:0.state: %s", this.name, status);
+                } else if (ds && ds.relays && ds.relays[0] && ds.relays[0].ison !== undefined) {
+                    status = ds.relays[0].ison;
+                    this.log("[%s] Using relays[0].ison: %s", this.name, status);
+                } else if (ds && ds['switch:0'] && ds['switch:0'].output !== undefined) {
+                    status = ds['switch:0'].output;
+                    this.log("[%s] Using switch:0.output: %s", this.name, status);
+                }
+
+                this.log("[%s] FINAL STATUS: %s", this.name, status);
                 callback(null, status);
+
             } catch (parseErr) {
                 this.log("[%s] Error parsing status JSON: %s", this.name, parseErr.message);
                 callback(parseErr);
             }
         });
     },
-    
+
     pollStatus: function() {
         if (this.debug) {
             this.log("[%s] Getting status: %s channel=%s&id=%s&auth_key=...", 
                 this.name, this.statusCloudURL, this.channel, this.deviceId);
         }
-        
+
         this.getStatus((err, status) => {
             if (!err && status !== undefined) {
                 const currentState = status === true ? Characteristic.CurrentDoorState.OPEN : Characteristic.CurrentDoorState.CLOSED;
                 this.service.getCharacteristic(Characteristic.CurrentDoorState).updateValue(currentState);
-                this.service.getCharacteristic(Characteristic.TargetDoorState).updateValue(currentState === Characteristic.CurrentDoorState.OPEN ? Characteristic.TargetDoorState.OPEN : Characteristic.TargetDoorState.CLOSED);
-                this.log("[%s] Status: %s -> HomeKit: %s", this.name, status, currentState === Characteristic.CurrentDoorState.OPEN ? "OPEN (0)" : "CLOSED (1)");
+                this.service.getCharacteristic(Characteristic.TargetDoorState).updateValue(
+                    currentState === Characteristic.CurrentDoorState.OPEN ? 
+                    Characteristic.TargetDoorState.OPEN : Characteristic.TargetDoorState.CLOSED
+                );
+                this.log("[%s] Status: %s -> HomeKit: %s", this.name, status, 
+                    currentState === Characteristic.CurrentDoorState.OPEN ? "OPEN (0)" : "CLOSED (1)");
             }
         });
     }
 };
+
